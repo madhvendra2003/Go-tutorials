b92faca28b0be6dd1a335dc4ab97dba177d3e753
Madhvendra Singh <madvendra1789@gmail.com>
2024-10-25 17:21:24 +0530

    input and comma operator

---
+++ User_Input/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Madhvendra singh"
+	fmt.Printf(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the reading for our pizza : ")
+
+	// comma ok // err ok
+
+	input, _ := reader.ReadString('\n')
+	fmt.Println("thanks for rating, ", input)
+	fmt.Printf("type of rating, %T", input)
+
+}
